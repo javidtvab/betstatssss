d684424652893df3353a15705f0c615366062615
javidtvab <165400423+javidtvab@users.noreply.github.com>
2025-01-04 20:42:47 +0100

    Update UserDashboard.js

--- src/components/User/UserDashboard.js
+++ src/components/User/UserDashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Profile from './Profile';
 import PicksList from './PicksList';
 import UserStatistics from './UserStatistics';
+import CreatePick from './CreatePick';
 
 const UserDashboard = () => {
   return (
@@ -9,6 +10,7 @@ const UserDashboard = () => {
       <h1 className="text-2xl font-bold mb-4">Panel de Usuario</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <Profile />
+        <CreatePick />
         <PicksList />
         <UserStatistics />
         {/* Añadir otros componentes aquí */}
