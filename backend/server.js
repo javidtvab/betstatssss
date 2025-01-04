7dac0dcf7b0c8c919b8a4645409caf4298bcf69f
javidtvab <165400423+javidtvab@users.noreply.github.com>
2025-01-04 19:19:53 +0100

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Rutas de Administración
+const adminRoutes = require('./routes/admin');
+app.use('/admin', adminRoutes);
+
+// Conectar a MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("Conexión a MongoDB establecida exitosamente");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto: ${port}`);
+});
