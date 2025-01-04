cbe5d0e7555e5d45a75257107ab4d4adac43394c
javidtvab <165400423+javidtvab@users.noreply.github.com>
2025-01-04 20:41:15 +0100

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { getProfile, updateProfile } = require('../controllers/userController');
 const { getUserPicks, getUserStatistics } = require('../controllers/userStatisticsController');
+const { createPick } = require('../controllers/userPickController');
 const upload = require('../middleware/upload');
 const auth = require('../middleware/auth'); // Middleware de autenticación
 
@@ -11,5 +12,8 @@ router.put('/profile', auth, upload.single('profilePhoto'), updateProfile);
 // Rutas de estadísticas
 router.get('/picks', auth, getUserPicks);
 router.get('/statistics', auth, getUserStatistics);
+
+// Rutas de creación de picks
+router.post('/picks', auth, upload.single('photo'), createPick);
 
 module.exports = router;
