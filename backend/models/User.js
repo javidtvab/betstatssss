294e9b0eb1f97dfb7b011198d50c71020537c51b
javidtvab <165400423+javidtvab@users.noreply.github.com>
2025-01-07 14:35:29 +0100

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -10,18 +10,20 @@ const socialMediaSchema = new mongoose.Schema({
 
 // Esquema principal de usuario
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true }, // Nombre de usuario, requerido y único
-  email: { type: String, required: true, unique: true }, // Correo electrónico, requerido y único
-  password: { type: String, required: true }, // Contraseña, requerida
-  profilePhoto: { type: String }, // URL de la foto de perfil
-  isBlocked: { type: Boolean, default: false }, // Indicador de si el usuario está bloqueado, por defecto es falso
-  role: { type: String, enum: ['basic', 'premium', 'admin'], default: 'basic' }, // Rol del usuario, con valores posibles 'basic', 'premium', 'admin'. Por defecto es 'basic'
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePhoto: { type: String },
+  isBlocked: { type: Boolean, default: false },
+  role: { type: String, enum: ['basic', 'premium', 'admin'], default: 'basic' },
   // Campos adicionales para usuarios premium
-  botAccess: { type: Boolean, default: false }, // Acceso a bot, por defecto es falso
-  enhancedStatistics: { type: Boolean, default: false }, // Acceso a estadísticas mejoradas, por defecto es falso
-  extendedProfile: { type: Boolean, default: false }, // Acceso a perfil extendido, por defecto es falso
-  socialMedia: socialMediaSchema, // Redes sociales del usuario
-  description: { type: String }, // Descripción del usuario y sus servicios
-}, { timestamps: true }); // Incluye campos createdAt y updatedAt automáticamente
+  botAccess: { type: Boolean, default: false },
+  enhancedStatistics: { type: Boolean, default: false },
+  extendedProfile: { type: Boolean, default: false },
+  socialMedia: socialMediaSchema,
+  description: { type: String },
+  telegramChannel: { type: String }, // Nombre o ID del canal de Telegram
+  telegramBotToken: { type: String }, // Token del bot de Telegram
+}, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema); // Exporta el modelo de usuario
+module.exports = mongoose.model('User', userSchema);
