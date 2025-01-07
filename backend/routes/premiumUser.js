c3327fcaa44b6ea6de33c7695c24a2d115f64e3b
javidtvab <165400423+javidtvab@users.noreply.github.com>
2025-01-07 14:32:14 +0100

    Update premiumUser.js

--- backend/routes/premiumUser.js
+++ backend/routes/premiumUser.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { getProfile, updateProfile } = require('../controllers/premiumUserProfileController');
 const { getUserPicks, getUserStatistics } = require('../controllers/premiumUserStatisticsController');
 const { createPick } = require('../controllers/premiumUserPickController');
+const { getSoldSubscriptions, getSoldPicks } = require('../controllers/premiumUserSalesController');
 const upload = require('../middleware/upload');
 const auth = require('../middleware/auth'); // Middleware de autenticación
 
@@ -15,5 +16,9 @@ router.get('/statistics', auth, getUserStatistics);
 
 // Rutas de creación de picks
 router.post('/picks', auth, upload.single('photo'), createPick);
+
+// Rutas de ventas
+router.get('/sales/subscriptions', auth, getSoldSubscriptions);
+router.get('/sales/picks', auth, getSoldPicks);
 
 module.exports = router;
