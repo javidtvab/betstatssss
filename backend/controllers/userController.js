700d2d2faa5270bc3d0a9a4e34d01d194581602e
javidtvab <165400423+javidtvab@users.noreply.github.com>
2025-01-07 13:53:27 +0100

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,31 +46,6 @@ const deleteUser = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-  // Obtener el perfil del usuario
-const getProfile = async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id);
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-// Actualizar el perfil del usuario
-const updateProfile = async (req, res) => {
-  const { username } = req.body;
-  let profilePhoto;
-  if (req.file) {
-    profilePhoto = await uploadFile(req.file);
-  }
-
-  try {
-    const updatedUser = await User.findByIdAndUpdate(req.user.id, { username, profilePhoto }, { new: true });
-    res.status(200).json(updatedUser);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
 };
 
 module.exports = { getUsers, createUser, updateUser, deleteUser };
